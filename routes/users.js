bebea4bbbf97f4bbcc04ff983cc5bf88361e571e
paula-melero <paula.melero25@gmail.com>
2020-04-20 10:39:02 -0400

    removed repeat-pwd requirement for creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const _ = require("lodash");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
 const router = express.Router();
-const auth = require("../middleware/auth");
-const asyncMiddleware = require("../middleware/async");
-const admin = require("../middleware/admin");
-const { User, validateUser } = require("../models/users");
+const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async');
+const admin = require('../middleware/admin');
+const { User, validateUser } = require('../models/users');
 
 //GET ALL USERS
 router.get(
-  "/",
+  '/',
   [auth, admin],
   asyncMiddleware(async (req, res) => {
     const users = await User.find()
       .sort({ username: 1 })
-      .select("-password")
+      .select('-password')
       .skip((req.query.pageNumber - 1) * req.query.pageSize)
       .limit(parseInt(req.query.pageSize));
 
@@ -24,16 +24,16 @@ router.get(
 
 //GET CURRENTLY LOGGED IN USER
 router.get(
-  "/me",
+  '/me',
   auth,
   asyncMiddleware(async (req, res) => {
-    console.log("reached get one user");
-    const user = await User.findById({ _id: req.user._id }).select("-password");
+    console.log('reached get one user');
+    const user = await User.findById({ _id: req.user._id }).select('-password');
 
     if (!user)
       return res
         .status(404)
-        .json({ message: "User with the given ID was not found" });
+        .json({ message: 'User with the given ID was not found' });
 
     res.status(200).json(user);
   })
@@ -41,23 +41,19 @@ router.get(
 
 //REGISTER A USER
 router.post(
-  "/",
+  '/',
   asyncMiddleware(async (req, res) => {
     const { error } = validateUser(req.body);
 
     if (error) return res.status(400).json(error.details[0].message);
 
-    const { username, password, repeat_password } = req.body;
+    const { username, password } = req.body;
 
     let user = await User.findOne({ username });
 
     //check if username exists
     if (user)
-      return res.status(400).json({ message: "Username already exists" });
-
-    //check if passwords match
-    if (password !== repeat_password)
-      res.status(400).json({ message: "Passwords don't match" });
+      return res.status(400).json({ message: 'Username already exists' });
 
     //hash password
     const salt = await bcrypt.genSalt(10);
@@ -71,14 +67,14 @@ router.post(
     const token = user.generateAuthToken();
 
     //return token in HTTP header
-    res.header("x-auth-token", token);
-    res.status(200).json(_.pick(user, ["_id", "username"]));
+    res.header('x-auth-token', token);
+    res.status(200).json(_.pick(user, ['_id', 'username']));
   })
 );
 
 //UPDATE USER
 router.put(
-  "/:id",
+  '/:id',
   auth,
   asyncMiddleware(async (req, res) => {
     const { error } = validateUser(req.body);
@@ -89,7 +85,7 @@ router.put(
     if (req.user._id !== req.params.id)
       return res
         .status(401)
-        .json({ message: "Access denied. Cannot edit user with given ID." });
+        .json({ message: 'Access denied. Cannot edit user with given ID.' });
 
     const { username, password } = req.body;
 
@@ -100,7 +96,7 @@ router.put(
     const user = await User.findByIdAndUpdate(
       req.user._id,
       {
-        $set: { username, password: hash }
+        $set: { username, password: hash },
       },
       { new: true }
     );
@@ -108,7 +104,7 @@ router.put(
     if (!user)
       return res
         .status(404)
-        .json({ message: "User with the given ID was not found" });
+        .json({ message: 'User with the given ID was not found' });
 
     res.status(200).json(user);
   })
@@ -116,7 +112,7 @@ router.put(
 
 //DELETE USER
 router.delete(
-  "/:id",
+  '/:id',
   [auth, admin],
   asyncMiddleware(async (req, res) => {
     const result = await User.findByIdAndDelete(req.params.id);
@@ -124,7 +120,7 @@ router.delete(
     if (!result)
       return res
         .status(404)
-        .json({ message: "User with the given ID was not found" });
+        .json({ message: 'User with the given ID was not found' });
 
     res.status(200).json(result);
   })
