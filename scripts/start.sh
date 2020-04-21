415f3136b39ef62915a8e29d42b3541e1a73dcfb
paula-melero <paula.melero25@gmail.com>
2020-04-21 13:04:21 -0400

    removed comments from start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,15 +2,6 @@
 export NODE_ENV=production
 export jwtPrivateKey=1234
 
-# RANDOM_STR=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
-# DIRS=~/todoapp-*
-# mkdir ~/todoapp-$RANDOM_STR
-# TO_DELETE=~/todoapp-!("$RANDOM_STR")
-# if ((${#DIRS[@]})); then
-#     echo "Reached condition"
-#      rm -rm ~/todoapp-*
-# fi
-
 cd home/todoapp
 npm install
 npm start &
