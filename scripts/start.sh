169ae4bca03d21256ab12db5224e2ede4d05c8a1
paula-melero <paula.melero25@gmail.com>
2020-04-21 14:18:02 -0400

    added application stop hook and script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export NODE_ENV=production
 export jwtPrivateKey=1234
-cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
-rm -rf *-cleanup
 cd /home/todoapp
 npm install
 npm start &
---
+++ scripts/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pkill node
+cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
+rm -rf *-cleanup
