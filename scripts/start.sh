c18137ed0a4666148988b8dad08f00ba8dbd719d
paula-melero <paula.melero25@gmail.com>
2020-04-21 14:05:32 -0400

    removing -cleanup file at start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export NODE_ENV=production
 export jwtPrivateKey=1234
-
+cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
+rm -rf *-cleanup
 cd /home/todoapp
 npm install
 npm start &
