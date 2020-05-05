920699be448d01cd9e083ee70bba0d86abfe8e1c
paula-melero <paula.melero25@gmail.com>
2020-05-05 14:50:52 -0400

    cleaned up public folder

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const index_path = path.join(__dirname, "/public");
+const path = require('path');
+const serve_static = require('serve-static');
+const index_path = path.join(__dirname, '/public/index.html');
+const css_path = path.join(__dirname, '/public/style.css');
 
-require("./startup/routes")(app);
-require("./startup/db")(process.env);
-require("./startup/logging")();
-require("./startup/config")();
+require('./startup/routes')(app);
+require('./startup/db')(process.env);
+require('./startup/logging')();
+require('./startup/config')();
 
-app.get("/", (req, res) => {
-  res.sendFile(index_path);
-});
+app.use(express.static('public'));
 
 module.exports = app;
