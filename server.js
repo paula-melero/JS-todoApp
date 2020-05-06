a2b2af49cb005cca192c38266355fc92665bbfdc
paula-melero <paula.melero25@gmail.com>
2020-05-06 11:49:08 -0400

    added git clone for user data and new health route

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const serve_static = require('serve-static');
-const index_path = path.join(__dirname, '/public/index.html');
-const css_path = path.join(__dirname, '/public/style.css');
 
 require('./startup/routes')(app);
 require('./startup/db')(process.env);
